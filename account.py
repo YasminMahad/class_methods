57b8d6d68ed5cd4115fb88672368dba26d21af27
YasminMahad <yasminmahadabdullahi@gmail.com>
2022-06-10 21:27:18 +0300

    class Account

--- account.py
+++ account.py
@@ -1,16 +1,45 @@
+from this import s
+
+
 class Account :
-    def __init__(self,account_name,account_number,pin,old_balance,balance):
+    def __init__(self,account_name,account_number):
         self.account_name = account_name
         self.account_number = account_number
-        self.pin = pin
-        self.old_balance = old_balance
-        self.balance = balance
+        self.balance = 0
+        self.deposits = []
+        self.withdrawals = []
     
-    def deposite(self):
-        self.old_balance += self.balance
-        return f"Hello {self.account_name}, your deposite is {self.old_balance}"
+    def deposite(self,amount):
+        self.balance+=amount
+        if amount<=0:
+            return f"Deposite must me positive amount"
+        else :
+            self.balance += amount   
+            self.deposits.append(amount) 
+            print(self.deposits)
+            return f"Your balance is {self.balance}"
 
-    def withdraw(self):
-        self.old_balance -= self.balance
-        return f"Hello {self.account_name}, your withdrawal is {self.old_balance}"
+    def withdraw(self,amount):
+        self.transaction=100
+        if amount <=0:
+          return f"Hello {self.account_name}, you cannot  withdraw is {amount}"
+        
+        elif amount>self.balance:
+            return f"You can not withdraw {amount} your balance is {self.balance}"
+        else:
+            self.balance-=amount+self.transaction
+            self.withdrawals.append(amount)
+            print(self.withdrawals)
+            return f"Hello {self.account_name}, you have withdrawn {amount}. Your new balance is {self.balance}"
     
+    def deposits_statement(self):
+        for x in self.deposits:
+            print(x,end="\n")
+    
+    def withdrawals_statement(self):
+        for w in self.withdrawals:
+            print(w,end="\n")
+
+# Add a method to show the current balance.
+    def current_balance(self):
+        return self.balance
